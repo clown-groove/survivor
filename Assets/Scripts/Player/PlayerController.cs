76b08791353eadd8dc1f564886a72233d661b283
Anirnir <anirnir.f@gmail.com>
2026-06-12 12:55:27 +0200

    Player controller progress
    Początek orpacy nad skryptem player controllera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,85 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerWeapon))]
+[RequireComponent(typeof(PlayerStats))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D rb;
+    private PlayerWeapon playerWeapon;
+    private PlayerStats playerStats;
+
+    private InputActionMap player;
+    private InputAction movement;
+    private InputAction attack;
+
+    private bool autoAiming;
+
+
+    #region Input events
+    private void OnPause(InputAction.CallbackContext obj)
+    {
+
+    }
+
+    private void OnReload(InputAction.CallbackContext obj)
+    {
+
+    }
+
+    private void OnAutoAim(InputAction.CallbackContext obj)
+    {
+
+    }
+    #endregion
+
+    #region Callbacks
+    private void Awake()
+    {
+        player = GetComponent<PlayerInput>().actions.FindActionMap("Player");
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerWeapon = GetComponent<PlayerWeapon>();
+        playerStats = GetComponent<PlayerStats>();
+
+        autoAiming = false;
+    }
+
+    private void OnEnable()
+    {
+        player.FindAction("Pause").performed += OnPause;
+        player.FindAction("Pause").Enable();
+        player.FindAction("Reload").performed += OnReload;
+        player.FindAction("Reload").Enable();
+        player.FindAction("AutoAim").performed += OnAutoAim;
+        player.FindAction("AutoAim").Enable();
+
+        movement = player.FindAction("Move");
+        movement.Enable();
+        attack = player.FindAction("Attack");
+        attack.Enable();
+    }
+
+    private void OnDisable()
+    {
+        player.FindAction("Pause").Disable();
+        player.FindAction("Reload").Disable();
+        player.FindAction("AutoAim").Disable();
+        movement.Disable();
+        attack.Disable();
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         
     }
+    #endregion
 }
