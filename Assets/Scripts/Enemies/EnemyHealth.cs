1a5c4dd771b47ce2d46d3d462eeb2d5fe9dc8bee
Anirnir <anirnir.f@gmail.com>
2026-06-20 12:14:05 +0200

    Enemies damage taken indicator
    Dodano zmianę koloru sprite'a przeciwnika po otrzymaniu przez niego obrażeń i szybki fade out. Dodatkowo fix hitboxów pocisku gracza

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(EnemyController))]
@@ -5,12 +7,37 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     private float currentHealth;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Color damagedColor = Color.rebeccaPurple;
+    private float colorFadeDuration = .2f;
+    private Coroutine fadeCoroutineReference;
 
     public void ApplyDamage(float damage)
     {
         currentHealth -= damage;
 
+        if(fadeCoroutineReference != null)
+        {
+            StopCoroutine(fadeCoroutineReference);
+        }
+        fadeCoroutineReference = StartCoroutine(ColorFadeCoroutine());
+
         CheckDeath();
+    }
+
+    private IEnumerator ColorFadeCoroutine()
+    {
+        float timer = 0;
+        while (timer < colorFadeDuration)
+        {
+            spriteRenderer.color = Color.Lerp(damagedColor, Color.white, timer / colorFadeDuration);
+            yield return new WaitForNextFrameUnit();
+            timer += Time.deltaTime;
+        }
+        spriteRenderer.color = Color.white;
+        fadeCoroutineReference = null;
     }
 
     private void CheckDeath()
