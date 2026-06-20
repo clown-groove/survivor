1b06322bd43caa7ae3ef054cf573354b0b79939b
Anirnir <anirnir.f@gmail.com>
2026-06-21 01:19:06 +0200

    colission fix
    naprawiona bufga z źle naliczającą sie kolizją

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -101,7 +101,7 @@ public class EnemyController : MonoBehaviour
         HandleMovement();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.rigidbody.GetComponent<PlayerHealth>())
         {
